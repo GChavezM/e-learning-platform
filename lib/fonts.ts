ad479e4f124ff711f219129737ea4003e93ec9c6
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-02-27 00:26:46 -0400

    Add fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,9 @@
+import { Space_Grotesk, Noto_Sans } from 'next/font/google';
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+});
+
+export const notoSans = Noto_Sans({
+  subsets: ['latin'],
+});
