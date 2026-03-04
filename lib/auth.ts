85d0594d0438fe6259c25b86cc1ae458bd873e58
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-03 22:20:41 -0400

    Add cookies support for server side

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import prisma from '@/lib/prisma';
+import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -9,5 +10,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()],
   trustedOrigins: [process.env['BETTER_AUTH_URL']!],
 });
