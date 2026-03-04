6d25d9565247fc4920c0253ddea10986914b202c
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-04 10:20:06 -0400

    Fix typo

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,16 +5,16 @@ const adapter = new PrismaPg({
   connectionString: process.env['DATABASE_URL']!,
 });
 
-const globaForPrisma = global as unknown as {
+const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 const prisma =
-  globaForPrisma.prisma ||
+  globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
   });
 
-if (process.env.NODE_ENV !== 'production') globaForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
