b1d3cb2f3ecbe7f1928618db16319a3cdbde3cae
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-10 17:39:35 -0400

    Add font variables

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,8 +2,10 @@ import { Space_Grotesk, Noto_Sans } from 'next/font/google';
 
 export const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
+  variable: '--font-space-grotesk',
 });
 
 export const notoSans = Noto_Sans({
   subsets: ['latin'],
+  variable: '--font-noto-sans',
 });
