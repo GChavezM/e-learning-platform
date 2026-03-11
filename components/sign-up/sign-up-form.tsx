ee26eb9bdbf39a67c9b5df7bbc54c55be7ceb108
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-10 20:57:45 -0400

    Add functionality to sign up form

--- components/sign-up/sign-up-form.tsx
+++ components/sign-up/sign-up-form.tsx
@@ -1,39 +1,39 @@
 'use client';
 
 import { useState } from 'react';
-// import { useRouter } from 'next/navigation';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Card } from '../ui/card';
 import { Field, FieldError, FieldGroup, FieldLabel } from '../ui/field';
-import * as z from 'zod';
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from '../ui/input-group';
 import { EyeIcon, EyeOffIcon, Loader2, Lock, Mail, Send, User } from 'lucide-react';
 import { Button } from '../ui/button';
 import Link from 'next/link';
-
-const formSchema = z.object({
-  username: z.string().min(2).max(100),
-  email: z.string().email(),
-  password: z.string().min(8).max(100),
-});
+import { SignUpFormData, signUpSchema } from '@/modules/auth/schemas';
+import { signUpAction } from '@/modules/auth/actions';
+import { toast } from 'sonner';
 
 export default function SignUpForm() {
   const [showPassword, setShowPassword] = useState(false);
-  //   const router = useRouter();
-  const form = useForm({
-    resolver: zodResolver(formSchema),
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const form = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
     mode: 'onBlur',
     defaultValues: {
-      username: '',
+      name: '',
       email: '',
       password: '',
+      confirmPassword: '',
     },
   });
 
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    console.log('Form Data:', data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const onSubmit = async (data: SignUpFormData) => {
+    const result = await signUpAction(data);
+
+    if (!result.success) {
+      toast.error(result.error);
+    }
   };
 
   return (
@@ -45,12 +45,12 @@ export default function SignUpForm() {
       >
         <FieldGroup>
           <Controller
-            name="username"
+            name="name"
             control={form.control}
             render={({ field, fieldState }) => (
               <Field data-invalid={fieldState.invalid}>
                 <FieldLabel htmlFor={field.name} className="text-slate-600 dark:text-slate-300">
-                  User Name
+                  Full Name
                 </FieldLabel>
                 <InputGroup className="focus-within:border-primary dark:focus-within:border-primary focus-within:ring-primary dark:focus-within:ring-primary group rounded-lg border-slate-300 bg-slate-50 transition-all duration-200 hover:border-slate-400 focus:ring-1 focus:outline-none dark:border-[#3d524c] dark:bg-[#111716]/80 dark:hover:border-[#536e66]">
                   <InputGroupInput
@@ -60,7 +60,7 @@ export default function SignUpForm() {
                     placeholder="Choose a user name"
                     aria-invalid={fieldState.invalid}
                     disabled={form.formState.isSubmitting}
-                    autoComplete="off"
+                    autoComplete="name"
                     required
                   />
                   <InputGroupAddon align="inline-start">
@@ -105,7 +105,9 @@ export default function SignUpForm() {
             control={form.control}
             render={({ field, fieldState }) => (
               <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                <FieldLabel htmlFor={field.name} className="text-slate-600 dark:text-slate-300">
+                  Password
+                </FieldLabel>
                 <InputGroup className="focus-within:border-primary dark:focus-within:border-primary focus-within:ring-primary dark:focus-within:ring-primary group rounded-lg border-slate-300 bg-slate-50 transition-all duration-200 hover:border-slate-400 focus:ring-1 focus:outline-none dark:border-[#3d524c] dark:bg-[#111716]/80 dark:hover:border-[#536e66]">
                   <InputGroupInput
                     {...field}
@@ -128,6 +130,43 @@ export default function SignUpForm() {
                       size="icon-xs"
                     >
                       {showPassword ? <EyeOffIcon /> : <EyeIcon />}
+                    </InputGroupButton>
+                  </InputGroupAddon>
+                </InputGroup>
+                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              </Field>
+            )}
+          />
+          <Controller
+            name="confirmPassword"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name} className="text-slate-600 dark:text-slate-300">
+                  Confirm Password
+                </FieldLabel>
+                <InputGroup className="focus-within:border-primary dark:focus-within:border-primary focus-within:ring-primary dark:focus-within:ring-primary group rounded-lg border-slate-300 bg-slate-50 transition-all duration-200 hover:border-slate-400 focus:ring-1 focus:outline-none dark:border-[#3d524c] dark:bg-[#111716]/80 dark:hover:border-[#536e66]">
+                  <InputGroupInput
+                    {...field}
+                    id={field.name}
+                    className="placeholder:text-slate-400 dark:placeholder:text-[#9eb7b1]"
+                    type={showPassword ? 'text' : 'password'}
+                    autoComplete="new-password"
+                    placeholder="Repeat your password"
+                    aria-invalid={fieldState.invalid}
+                    disabled={form.formState.isSubmitting}
+                    required
+                  />
+                  <InputGroupAddon align="inline-start">
+                    <Lock className="group-focus-within:text-primary dark:group-focus-within:text-primary text-slate-400 dark:text-[#9eb7b1]" />
+                  </InputGroupAddon>
+                  <InputGroupAddon align="inline-end">
+                    <InputGroupButton
+                      aria-label={showConfirmPassword ? 'Hide password' : 'Show password'}
+                      onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                      size="icon-xs"
+                    >
+                      {showConfirmPassword ? <EyeOffIcon /> : <EyeIcon />}
                     </InputGroupButton>
                   </InputGroupAddon>
                 </InputGroup>
