b535f58cd2103c84a1a3908e1aacd581280bc6e4
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-10 20:54:23 -0400

    Add proxy middleware

---
+++ proxy.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+
+export async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (pathname.startsWith('/dashboard')) {
+    if (!session) {
+      return NextResponse.redirect(new URL('sign-in', request.url));
+    }
+  }
+
+  if (pathname.startsWith('/sign-in') || pathname.startsWith('/sign-up')) {
+    if (session) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+};
