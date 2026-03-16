1e9c279951d7707a239ae5f39e83c6245f70c729
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-16 04:31:51 -0400

    Improve admin layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,7 +1,22 @@
 import { AppSidebar } from '@/components/admin/app-sidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const headersList = await headers();
+  const session = await auth.api.getSession({ headers: headersList });
+  const isGuestMode = headersList.get('x-guest-mode') === '1';
+
+  if (!session && !isGuestMode) {
+    redirect('/sign-in');
+  }
+
+  if (!session && isGuestMode) {
+    return <main className="w-full">{children}</main>;
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
